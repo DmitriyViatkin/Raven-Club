cbaf7a62b039032086129d07c26f55ce35130dc8
Dmitriy <ViatkinDima@gmail.com>
2026-05-28 19:44:46 +0300

    Fix registration

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from .forms import PlayerCreationForm
+
 
 class Register(View):
     template_name = 'authentication/register.html'
 
     def get(self, request):
-        context = {'form': UserCreationForm()}
+        context = {'form': PlayerCreationForm()}
         return render(request, self.template_name, context)
+
     def post(self, request):
-        form = UserCreationForm(request.POST)
+        form = PlayerCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
+            user = form.save()
             login(request, user)
             return redirect('/')
+
         context = {'form': form}
         return render(request, self.template_name, context)
